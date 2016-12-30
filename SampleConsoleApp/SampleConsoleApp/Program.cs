5ea424e8837f9f1294dbeba3d71a17f0f1940419
MettaVinod <mettavinodreddy@gmail.com>
2016-12-29 20:13:54 -0600

    check Detect

--- SampleConsoleApp/SampleConsoleApp/Program.cs
+++ SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace SampleConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I am vinodMetta no");
+            Console.WriteLine("I am vinodMetta");
             Console.ReadKey();
         }
     }
